f2ae1d1236d88d17cf7f5855b1281cc46eb6a634
Raul Victor <31050006+Raul772@users.noreply.github.com>
2024-05-30 14:37:19 -0300

    feature: initial setup

---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`[TechNexus-Backend]: Server Running at http://localhost:${port}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="pt-2 p-4">
+
+      </div>
+    </>
+  )
+}
+
+export default App
